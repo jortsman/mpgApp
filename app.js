0a775cc5baa89bf69764088aca0513f401ea7d6f
Vlad Pasculescu <vpasculescu@Vlads-MacBook-Pro.local>
2017-02-04 11:38:35 -0800

    Code/Language Fixes
    Fixed initial string in second prompt. Also, modified the if statement
    to include the else condition

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var again = "yes";
 do {
   var miles = prompt('How many miles did you drive? \n Make sure you enter a number.');
   parseFloat(miles);
-  var gallons = prompt('How many miles did you drive? \n Again, please enter a number.');
+  var gallons = prompt('How many gallons were used? \n Again, please enter a number.');
   parseFloat(gallons);
   if (!isNaN(miles) && miles > 0 && !isNaN(gallons) && gallons > 0) {
     var mpg = miles/gallons;
+    parseInt(mpg);
     alert('Your calculated miles per gallon is ' + mpg);
+  } else {
+    alert('One of your entries is incorrect.');
   }
-} while (again = "yes", "yes");
+  again = prompt('Do you want to try again?', 'yes');
+} while (again == "yes");
